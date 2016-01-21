05f64068db484d283ee43132905a863fe6c82b5f
janhajk <janhajk@gmail.com>
2016-01-21 12:09:18 +0000

    list movies

--- public/js/mp.js
+++ public/js/mp.js
@@ -4,28 +4,62 @@ $(document).ready(function() {
    };
    $("#formaddmovie").submit(function(event) {
       event.preventDefault();
-      addMovie();
+      movieAdd();
    });
-   var addMovie = function() {
-            var data = {
-               title: $('#addMovieTitle').val(),
-               imdbid: $('#addMovieimdbid').val(),
-               boxofficemojoid: $('#addMovieboxofficemojoid').val(),
-               runtime: $('#addMovieruntime').val(),
-               date_opening: $('#addMoviedate_opening').val(),
-               date_opening_us: $('#addMoviedate_opening_us').val(),
-               date_opening_china: $('#addMoviedate_opening_china').val(),
-               date_release_disc: $('#addMoviedate_release_disc').val()
-            };
-            $.ajax("/movie/add", {
-               type: "POST",
-               data: data,
-               dataType: "json",
-               success: function(text) {
-                  if(text == "success") {
-                     msg('New Movie created. ' + text);
-                  }
-               }
-            });
+   var movieAdd = function() {
+      var data = {
+         title: $('#addMovieTitle').val(),
+         imdbid: $('#addMovieimdbid').val(),
+         boxofficemojoid: $('#addMovieboxofficemojoid').val(),
+         runtime: $('#addMovieruntime').val(),
+         date_opening: $('#addMoviedate_opening').val(),
+         date_opening_us: $('#addMoviedate_opening_us').val(),
+         date_opening_china: $('#addMoviedate_opening_china').val(),
+         date_release_disc: $('#addMoviedate_release_disc').val()
       };
+      $.ajax("/movie/add", {
+         type: "POST",
+         data: data,
+         dataType: "json",
+         success: function(text) {
+            if(text == "success") {
+               msg('New Movie created. ' + text);
+            }
+         }
+      });
+   };
+   
+   var movieLoad = function(id, callback) {
+      if (id === 0 || typeof id === 'undefined') id = [];
+      if (id.constructor !== Array) id = [id];
+      $.ajax('/movie', {
+         type: 'GET',
+         data: {id:id},
+         dataType: 'json',
+         success: function(rows){
+            callback(rows);
+         }
+      });
+   };
+   
+   var movieLine = function(movie) {
+      var tr = document.createElement('tr');
+      var title = document.createElement('td');
+      var updateHistory = document.createElement('td');
+      title.innerHTML = movie.title;
+      updateHistory.innerHTML = 'update history';
+      updateHistory.onclick = function(){
+         movieHistoryUpdate(movie.id);
+      };
+      tr.appendChild(title);
+      tr.appendChild(updateHistory);
+      return tr;
+   };
+   movieLoad(0, function(rows){
+      var container = document.getElementById('movies');
+      var table = document.createElement('table');
+      for (var i in rows) {
+         table.appendChild(movieLine(rows[i]));
+      }
+   });
 });
